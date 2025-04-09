73578ca821f6c100d70a88f3e90138db53a4bebf
vinitdadyala <149586348+vinitdadyala@users.noreply.github.com>
2025-04-09 19:04:30 +0530

    Upgrade dependencies

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import com.google.common.primitives.Ints;
+
 public class Calculator {
     public int add(int a, int b) {
-        return a + b;
+        return Ints.saturatedAdd(a, b);
     }
 
     public int subtract(int a, int b) {
--- src/main/java/com/example/GuavaExample.java
+++ src/main/java/com/example/GuavaExample.java
@@ -1,9 +1,5 @@
-package com.example;
-
-import com.google.common.collect.ImmutableList;
-
 public class GuavaExample {
-    public ImmutableList<String> getImmutableList(String... elements) {
-        return ImmutableList.copyOf(elements);
+    public List<String> getImmutableList(String... elements) {
+        return List.of(elements);
     }
 }
