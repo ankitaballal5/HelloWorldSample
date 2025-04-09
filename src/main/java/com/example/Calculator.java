db7da90ceab377426564c219557fcedb6b7a6f32
vinitdadyala <149586348+vinitdadyala@users.noreply.github.com>
2025-04-09 17:55:05 +0530

    Upgrade dependencies

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import com.google.common.primitives.Ints;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
 
     public int subtract(int a, int b) {
-        return a - b;
+        return Ints.difference(a, b).intValue();
     }
 }
