91e74a7b347dfc82250d769540f028df2dac7b68
vinitdadyala <149586348+vinitdadyala@users.noreply.github.com>
2025-04-10 11:35:05 +0530

    Upgrade dependencies

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import com.google.common.collect.ImmutableSet;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
--- src/main/java/com/example/StringUtilsExample.java
+++ src/main/java/com/example/StringUtilsExample.java
@@ -1,13 +1,16 @@
 package com.example;
 
 import org.apache.commons.lang3.StringUtils;
+import java.nio.CharBuffer;
 
 public class StringUtilsExample {
     public boolean isPalindrome(String input) {
-        if (StringUtils.isEmpty(input)) {
+        if (input == null || input.isEmpty()) {
             return false;
         }
-        String reversed = StringUtils.reverse(input);
+        CharBuffer charBuffer = CharBuffer.wrap(input.toCharArray());
+        charBuffer.reverse();
+        String reversed = charBuffer.toString();
         return input.equals(reversed);
     }
 }
