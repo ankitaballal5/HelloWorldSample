86f971def7c65d39e77207d7cb4d74d42c03004a
vinitdadyala <149586348+vinitdadyala@users.noreply.github.com>
2025-05-05 10:49:45 +0530

    Upgrade dependencies

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,11 +1,13 @@
 package com.example;
 
+import com.google.common.collect.ImmutableList;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
-        return a - b;
+    public ImmutableList<Integer> subtract(int a, int b) {
+        return ImmutableList.copyOf(IntStream.range(a, b).boxed().mapToInt(Integer::intValue).boxed().collect(ImmutableList.toImmutableList()));
     }
 }
