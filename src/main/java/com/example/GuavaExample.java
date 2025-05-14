4c23a0c003d441ad0bb4a786529017f091b475d2
vinitdadyala <149586348+vinitdadyala@users.noreply.github.com>
2025-05-14 15:33:36 +0530

    Upgrade dependencies

--- src/main/java/com/example/GuavaExample.java
+++ src/main/java/com/example/GuavaExample.java
@@ -1,9 +1,11 @@
 package com.example;
 
 import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GuavaExample {
     public ImmutableList<String> getImmutableList(String... elements) {
-        return ImmutableList.copyOf(elements);
+        return ImmutableList.copyOf(List.of(elements));
     }
 }
